349b65d67e476886090d4cc08e7168b65b68915d
RYAN WILLMOT <rwillmo@purdue.edu>
2014-10-25 20:59:29 -0400

    Three of Four checks in isSolved Method

--- Sudoku.java
+++ Sudoku.java
@@ -14,9 +14,6 @@ public class Sudoku {
     
     // Returns a copy of the current state of the board--Done
     public int[][] board() {
-        int[][] copyboard = new int[9][9];
-        
-      
         return Arrays.copyOf(board, board.length);
     }
     
@@ -26,6 +23,30 @@ public class Sudoku {
     }
     
     public boolean isSolved() {
+       
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                // Board is not solved if there are any 0s in the board
+                if (board[i][j] == 0)
+                    return false;
+                
+                // Board is not solved if there is a similar number in a row
+                for (int k = j; k < 9; k++) {
+                    if (board[i][j] == board[i][k])
+                        return false;
+                }
+                
+                // Board is not solved if there is a similar number in column
+                for (int k = i; k < 9; k++) {
+                    if (board[j][i] == board[k][i])
+                        return false;
+                }
+            }
+        }
+        
+        
+        
+        
         return true;
     }
     
