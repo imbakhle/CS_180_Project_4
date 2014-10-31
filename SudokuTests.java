0fe701b89c005b90c4d56480af6e981341b048c9
Ishaan Bakhle <imbakhle@gmail.com>
2014-10-31 10:00:51 -0400

    Added tests for board(), candidates(), and isSolved()

--- SudokuTests.java
+++ SudokuTests.java
@@ -18,14 +18,38 @@ public class SudokuTests extends TestCase {
             {0,0,0,0,0,0,0,0,0},{3,2,0,6,0,0,0,0,0},{0,0,0,0,5,0,0,4,0},
             {7,0,0,0,0,0,0,0,0},{0,0,0,2,0,0,3,0,0},{4,8,0,0,0,0,0,0,0},
             {5,0,1,0,0,0,0,0,0}};
-        Sudoku s = new Sudoku();
-        s.main(new String[] {"0000410000600002000000000003206000"
-            + "00000050040700000000000200300480000000501000000"});
+        Sudoku s = new Sudoku(board);
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
                 assertEquals(s.board()[i][j], board[i][j]);
             }
         }
     }
+    
+    public void testCandidates() {
+        boolean[] cellCandidates = {false, false, false, true, true, true,
+            false, false, false, true};
+        
+        int[][] board = {{0,2,8,0,0,7,0,0,0},{0,1,6,0,0,0,2,0,0},
+            {0,0,0,0,0,0,0,0,0},{1,2,0,6,0,0,0,0,0},{0,0,0,0,5,0,0,4,0},
+            {0,0,0,0,0,0,0,0,0},{2,0,0,2,0,0,3,0,0},{0,8,0,0,0,0,0,0,0},
+            {0,0,1,0,0,0,0,0,0}};
+        Sudoku s = new Sudoku(board);
+        
+        for (int i = 0; i < 10; i++) {
+            assertEquals(s.candidates(0,0)[i], cellCandidates[i]);
+        }
+    }
+    
+    public void testIsSolved() {
+        int[][] board = {{0,2,8,0,0,7,0,0,0},{0,1,6,0,0,0,2,0,0},
+            {0,0,0,0,0,0,0,0,0},{1,2,0,6,0,0,0,0,0},{0,0,0,0,5,0,0,4,0},
+            {0,0,0,0,0,0,0,0,0},{2,0,0,2,0,0,3,0,0},{0,8,0,0,0,0,0,0,0},
+            {0,0,1,0,0,0,0,0,0}};
+        Sudoku s = new Sudoku(board);
+        
+        assertEquals(s.isSolved(), false);
+    }
+        
     
 }
