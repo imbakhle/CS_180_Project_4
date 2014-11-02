ab993223e4a7fd9d558c2f785de1bf1efe7162cf
RYAN WILLMOT <rwillmo@purdue.edu>
2014-11-01 22:37:42 -0400

    Made Changes

--- SudokuTests.java
+++ SudokuTests.java
@@ -53,6 +53,7 @@ public class SudokuTests extends TestCase {
         }
     }
     
+    // Tests if isSolved returns false for empty cell
     public void testisSolved() {
         int[][] board = {{0, 2, 8, 0, 0, 7, 0,  0, 0}, {0, 1, 6, 0, 0, 0, 2, 0, 0},
             {0, 0, 0, 0, 0, 0, 0, 0, 0}, {1, 2, 0, 6, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 5, 0, 0, 4, 0},
@@ -63,7 +64,7 @@ public class SudokuTests extends TestCase {
         assertEquals(s.isSolved(), false);
     }
     
-    // Tests if isSolved returns true
+    // Tests if isSolved returns true for complete board
     public void test2isSolved() {
         int[][] board = {{2, 9, 6, 3, 1, 8, 5, 7, 4}, {5, 8, 4, 9, 7, 2, 6, 1, 3},
             {7, 1, 3, 6, 4, 5, 2, 8, 9}, {6, 2, 5, 8, 9, 7, 3, 4, 1}, {9, 3, 1, 4, 2, 6, 8, 5, 7},
@@ -75,8 +76,9 @@ public class SudokuTests extends TestCase {
         
     }
     
+    // Tests if isSolved returns false for similar number in row
     public void test3isSolved() {
-        int[][] board = {{2, 2, 6, 3, 1, 8, 5, 7, 4}, {5, 8, 4, 9, 7, 2, 6, 1, 3},
+        int[][] board = {{2, 9, 6, 3, 1, 2, 5, 7, 4}, {5, 8, 4, 9, 7, 2, 6, 1, 3},
             {7, 1, 3, 6, 4, 5, 2, 8, 9}, {6, 2, 5, 8, 9, 7, 3, 4, 1}, {9, 3, 1, 4, 2, 6, 8, 5, 7},
             {4, 7, 8, 5, 3, 1, 9, 2, 6}, {1, 6, 7, 2, 5, 3, 4, 9, 8}, {8, 5, 9, 7, 6, 4, 1, 3, 2},
             {3, 4, 2, 1, 8, 9, 7, 6, 5}};
@@ -86,6 +88,19 @@ public class SudokuTests extends TestCase {
         
     }
     
+    // Tests if isSolved returns false for similar number in column
+    public void test4isSolved() {
+        int[][] board = {{2, 9, 6, 3, 1, 8, 5, 7, 4}, {5, 8, 4, 9, 7, 2, 6, 1, 3},
+            {2, 1, 3, 6, 4, 5, 2, 8, 9}, {6, 2, 5, 8, 9, 7, 3, 4, 1}, {9, 3, 1, 4, 2, 6, 8, 5, 7},
+            {4, 7, 8, 5, 3, 1, 9, 2, 6}, {1, 6, 7, 2, 5, 3, 4, 9, 8}, {8, 5, 9, 7, 6, 4, 1, 3, 2},
+            {3, 4, 2, 1, 8, 9, 7, 6, 5}};
+        Sudoku s = new Sudoku(board);
+        
+        assertEquals(s.isSolved(), false);
+        
+    }
+    
+    // Tests nakedSingles 
     public void testnakedSingles() {
         int[][] board = {{2, 9, 6, 3, 0, 8, 5, 7, 4}, {0, 0, 0, 0, 0, 0, 0, 0, 0},
             {0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0},
@@ -104,7 +119,20 @@ public class SudokuTests extends TestCase {
         }
     }
     
-    public void testHiddenSingles() {
+    // Tests hiddenSingles for column
+    public void testhiddenSingles() {
+        int[][] board = {{0, 0, 2, 1, 9, 3, 0, 0, 0}, {0, 0, 0, 0, 0, 7, 0, 0, 0},
+            {7, 0, 0, 0, 4, 0, 0, 1, 9}, {8, 0, 3, 0, 0, 0, 6, 0, 0}, {0, 4, 5, 0, 0, 0, 2, 3, 0},
+            {0, 0, 7, 0, 0, 0, 5, 0, 4}, {3, 7, 0, 0, 8, 0, 0, 0, 6}, {0, 0, 0, 6, 0, 0, 0, 0, 0},
+            {0, 0, 0, 5, 3, 4, 1, 0, 0}};
+        Sudoku s = new Sudoku(board);
+        
+        while (s.hiddenSingles()) { }
+        assertEquals(3, s.board()[5][3]);
+    }
+    
+    // Yests hiddenSingles for row
+    public void test2hiddenSingles() {
         int[][] board = {{0, 2, 8, 0, 0, 7, 0, 0, 0}, {0, 1, 6, 0, 8, 3, 0, 7, 0},
             {0, 0, 0, 0, 2, 0, 8, 5, 1}, {1, 3, 7, 2, 9, 0, 0, 0, 0}, {0, 0, 0, 7, 3, 0, 0, 0, 0},
             {0, 0, 0, 0, 4, 6, 3, 0, 7}, {2, 9, 0, 0, 7, 0, 0, 0, 0}, {0, 0, 0, 8, 6, 0, 1, 4, 0},
