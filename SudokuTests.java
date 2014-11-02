c15ad71caa30ad255d2fa5e4b7cfc77d7b7c8b0a
RYAN WILLMOT <rwillmo@purdue.edu>
2014-11-01 21:55:40 -0400

    Added more test to test unit

--- SudokuTests.java
+++ SudokuTests.java
@@ -10,6 +10,21 @@ public class SudokuTests extends TestCase {
     /**
      * A test method.
      */
+    
+    public void testSudoku() {  
+        Sudoku s = new Sudoku();
+        int[][] hello = {{0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0}};
+        
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                assertEquals(hello[i][j], s.board()[i][j]);
+            }
+        }
+    }
+    
     public void testBoard() {
         int[][] board = {{0, 0, 0, 0, 4, 1, 0, 0, 0}, {0, 6, 0, 0, 0, 0, 2, 0, 0},
             {0, 0, 0, 0, 0, 0, 0, 0, 0}, {3, 2, 0, 6, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 5, 0, 0, 4, 0},
@@ -38,7 +53,7 @@ public class SudokuTests extends TestCase {
         }
     }
     
-    public void testIsSolved() {
+    public void testisSolved() {
         int[][] board = {{0, 2, 8, 0, 0, 7, 0,  0, 0}, {0, 1, 6, 0, 0, 0, 2, 0, 0},
             {0, 0, 0, 0, 0, 0, 0, 0, 0}, {1, 2, 0, 6, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 5, 0, 0, 4, 0},
             {0, 0, 0, 0, 0, 0, 0, 0, 0}, {2, 0, 0, 2, 0, 0, 3, 0, 0}, {0, 8, 0, 0, 0, 0, 0, 0, 0},
@@ -48,7 +63,8 @@ public class SudokuTests extends TestCase {
         assertEquals(s.isSolved(), false);
     }
     
-    public void test2IsSolved() {
+    // Tests if isSolved returns true
+    public void test2isSolved() {
         int[][] board = {{2, 9, 6, 3, 1, 8, 5, 7, 4}, {5, 8, 4, 9, 7, 2, 6, 1, 3},
             {7, 1, 3, 6, 4, 5, 2, 8, 9}, {6, 2, 5, 8, 9, 7, 3, 4, 1}, {9, 3, 1, 4, 2, 6, 8, 5, 7},
             {4, 7, 8, 5, 3, 1, 9, 2, 6}, {1, 6, 7, 2, 5, 3, 4, 9, 8}, {8, 5, 9, 7, 6, 4, 1, 3, 2},
@@ -56,6 +72,17 @@ public class SudokuTests extends TestCase {
         Sudoku s = new Sudoku(board);
         
         assertEquals(s.isSolved(), true);
+        
+    }
+    
+    public void test3isSolved() {
+        int[][] board = {{2, 2, 6, 3, 1, 8, 5, 7, 4}, {5, 8, 4, 9, 7, 2, 6, 1, 3},
+            {7, 1, 3, 6, 4, 5, 2, 8, 9}, {6, 2, 5, 8, 9, 7, 3, 4, 1}, {9, 3, 1, 4, 2, 6, 8, 5, 7},
+            {4, 7, 8, 5, 3, 1, 9, 2, 6}, {1, 6, 7, 2, 5, 3, 4, 9, 8}, {8, 5, 9, 7, 6, 4, 1, 3, 2},
+            {3, 4, 2, 1, 8, 9, 7, 6, 5}};
+        Sudoku s = new Sudoku(board);
+        
+        assertEquals(s.isSolved(), false);
         
     }
     
